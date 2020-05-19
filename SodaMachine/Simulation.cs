8cb079f147930a14fe874e0f296b3da770829dab
Bill <william.hine2019@gmail.com>
2020-05-19 14:54:25 -0400

    Added ChooseASoda()

--- SodaMachine/Simulation.cs
+++ SodaMachine/Simulation.cs
@@ -20,6 +20,21 @@ namespace SodaMachine
             customer = new Customer();
         }
 
+        public List<Can> ChooseASoda()
+        {
+            List < Can > canChoice = new List<Can>();
+            int index = 0;
+
+            foreach(Can item in sodaMachineA.cans)
+            {
+                Console.WriteLine($"Choose {index}: for {item.name} Price: {item.Cost} ");
+                index++;
+            }
+            Console.WriteLine($"Enter choice:");
+            int userInput = int.Parse(Console.ReadLine());
+            canChoice.Add(sodaMachineA.cans[userInput]);
+            return canChoice;
+        }
         public List<Coin> SetAmountToDispense()
         {
             int index = 0;
@@ -33,6 +48,7 @@ namespace SodaMachine
             int coinCount = customer.wallet.coins.Count + 1;
             Console.WriteLine($"Enter {coinCount} when done selecting coins");
             int choice = int.Parse(Console.ReadLine());
+
 
             while (choice != coinCount) {
                 changeSelection.Add(customer.wallet.coins[choice]);
@@ -49,6 +65,7 @@ namespace SodaMachine
             Customer customer = new Customer();
 
             List<Coin> returnList = SetAmountToDispense();
+            List<Can>  sodaChoice = ChooseASoda();
             Console.ReadLine();
         }
     }
